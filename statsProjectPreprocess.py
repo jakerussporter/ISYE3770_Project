8d51f9c2d86cebf008c843b3a21d7cd19fdbcdd7
jakerussporter <55679464+jakerussporter@users.noreply.github.com>
2022-05-02 00:49:23 -0400

    Update files to separate hosted values from non hosted

--- statsProjectPreprocess.py
+++ statsProjectPreprocess.py
@@ -58,11 +58,25 @@ t_processed = t_processed.drop(diff, axis=1)
 
 
 host_countries = ['GRE', 'FRA', 'USA', 'GBR', 'SWE', 'BEL', 'SUI', 'NED', 'GER', 'NOR', 'FIN', 'ITA', 'AUS', 'AUT', 'JPN', 'MEX', 'FRG', 'CAN', 'URS', 'YUG', 'KOR', 'ESP', 'CHN', 'RUS', 'BRA']
+host_countries_years = [('GRE', 1896), ('FRA', 1900), ('USA', 1904), ('GBR', 1908), ('SWE', 1912), ('BEL', 1920), ('FRA', 1924), ('SUI', 1928), ('NED', 1928), ('USA', 1932), ('GER', 1936), ('SUI', 1948), ('GBR', 1948), ('NOR', 1952), ('FIN', 1952), ('ITA', 1956), ('AUS', 1956), ('USA', 1960), ('ITA', 1960), ('AUT', 1964), ('JPN', 1964), ('FRA', 1968), ('MEX', 1968), ('JPN', 1972), ('FRG', 1972), ('AUT', 1976), ('CAN', 1976), ('USA', 1980), ('URS', 1980), ('YUG', 1984), ('USA', 1984), ('CAN', 1988), ('KOR', 1988), ('FRA', 1992), ('ESP', 1992), ('NOR', 1994), ('USA', 1996), ('JPN', 1998), ('AUS', 2000), ('USA', 2002), ('GRE', 2004), ('ITA', 2006), ('CHN', 2008), ('CAN', 2010), ('GBR', 2012), ('RUS', 2014), ('BRA', 2016)]
 
 #Create rate table of each countries medals / attemps for each Olympics
 medal_rate = m_processed.div(t_processed)
 #medal_rate = medal_rate.fillna(0)
 medal_rate = medal_rate[host_countries]
+
+hosted_values = medal_rate.copy()
+hosted_values[:] = np.nan
+
+non_hosted_values = medal_rate.copy()
+
+for item in host_countries_years:
+    hosted_values.at[item[1], item[0]] = non_hosted_values.at[item[1], item[0]]
+    non_hosted_values.at[item[1], item[0]] = np.nan
+
+
+print(medal_rate)
+#print(non_hosted_values)
 
 #Turn the data into a csv file of the (medals earned) / (attempts at medals). If line df = df.drop_duplicates() on line
 #14 is uncommented, rename the file name below to 'medal_rate_no_teams.csv'.
@@ -70,3 +84,5 @@ medal_rate = medal_rate[host_countries]
 m_processed.to_csv('processed_teams.csv', index=True)
 t_processed.to_csv('attempts_teams.csv', index=True)
 medal_rate.to_csv('medal_rate.csv', index=True)
+hosted_values.to_csv('hosted_values.csv', index=True)
+non_hosted_values.to_csv('non_hosted_values.csv', index=True)
